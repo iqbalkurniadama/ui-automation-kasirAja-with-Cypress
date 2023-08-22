62cc9ec092694c55dffd7b0dedac9aa25686cd70
iqbalkurnia <iqbalkurnia091@gmail.com>
2023-08-22 22:33:29 +0700

    update report with html

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // mochawesome reports
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     // agar dapat jalan di browser: "chrome" gunakan cara dibawah ini untuk mendapatkan url,
     "chromeWebSecurity": false,
@@ -8,6 +17,13 @@ module.exports = defineConfig({
     "chromeWebSecurityCSP": false,
     
     // url
-    baseUrl: "https://kasirdemo.belajarqa.com"
+    baseUrl: "https://kasirdemo.belajarqa.com",
+
+    // mochawesome report options
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
+  
+
 });
